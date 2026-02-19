145a6486a56b8786ce4f9de6c8aee9101053c62b
Juan Santos Cotelo <juansantoscotelo@gmail.com>
2026-02-19 12:49:58 +0100

    Getters y constructores de la clase coche

---
+++ src/Coche.java
@@ -0,0 +1,25 @@
+public class Coche {
+    private double velocidadMax;
+    private String escudería;
+    private double fiabilidad;
+
+
+    public double getVelocidadMax() {
+        return velocidadMax;
+    }
+}
+
+    public String getEscudería() {
+        return escudería;
+    }
+
+    public double getFiabilidad() {
+        return fiabilidad;
+    }
+
+    public Coche(double velocidadMax, String escudería, double fiabilidad) {
+        this.piloto = piloto;
+        this.escudería = escudería;
+        this.fiabilidad = fiabilidad;
+    }
+}
