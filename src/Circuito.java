60f49ead8683d2762c71c91825c0f4d0f486982c
NoamNeo <gabriel.loira.lorenzo@gmail.com>
2026-02-19 13:29:20 +0100

    Eliminamos el cuerpo de los métodos de Circuito.java porque no eran necesarios

--- src/Circuito.java
+++ src/Circuito.java
@@ -3,22 +3,10 @@ public class Circuito {
   private int longitud;
   private Carrera[] carreras = null;
 
-  public int obtenerCarrerasTotales() {
-    return (carreras == null) ? 0 : carreras.length;
+  public void anhadirCarrera(Carrera carrera) {
   }
 
-  public void anhadirCarrera(Carrera carrera) {
-    int length = (carreras == null) ? 1 : carreras.length + 1;
-    Carrera[] buffer = new Carrera[length];
-    if (carreras == null) {
-      buffer[0] = carrera;
-      carreras = buffer;
-    } else {
-      for (int i = 0; i < carreras.length; i++) {
-        buffer[i] = carreras[i];
-      }
-      buffer[length - 1] = carrera;
-      carreras = buffer;
-    }
+  public int obtenerCarrerasTotales() {
+    return 0;
   }
 }
