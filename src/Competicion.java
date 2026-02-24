3c1fd28e977f3c28bfb1cfcef7495bd1a6c7a74f
NoamNeo <gabriel.loira.lorenzo@gmail.com>
2026-02-24 12:17:25 +0100

    Implementamos: imprimirResultado y imprimirResultadoEscuderia en Competicion.java
    Para ello creamos varios métodos privados y seguimos este proceso:
    1. Almacenamos la puntuación de cada piloto en un array de ints que
    coinciden en índice por cada piloto.
    2. Hacemos un insert sort para organizar un array de ids correspondiende
    a las ids de los pilotos.
    3. Creamos un array de Strings para las escuderías
    4. Almacenamos la puntuación de las escuderías (mismo índice) en un
    array de ints al que le sumamos la puntuación del piloto si su coche es
    de esa escudería

--- src/Competicion.java
+++ src/Competicion.java
@@ -3,6 +3,7 @@ public class Competicion {
     private boolean haCampeonato;
     private String nombre;
     private Piloto[] participantes;
+    private int[] puntuacionPilotos;
     private int numPilotos; // Contador para saber cuántos pilotos hay realmente
 
     private Carrera[] carreras;
@@ -10,13 +11,106 @@ public class Competicion {
 
     public boolean arrancarCampeonato() {
         if (!haCampeonato) {
+            this.puntuacionPilotos = new int[participantes.length];
             for (Carrera iCarrera : carreras) {
                 iCarrera.ejecutarCarrera(participantes);
+                almacenarResultados(iCarrera.getPilotosOrdenados());
             }
             this.haCampeonato = true;
             return true;
         }
         return false;
+    }
+
+    public void imprimirResultado() {
+        int[] idOrdenadas = new int[participantes.length];
+        for (int i = 0; i < idOrdenadas.length; i++) {
+            idOrdenadas[i] = i;
+        }
+        idOrdenadas = insertSort(idOrdenadas, puntuacionPilotos);
+        for (int i = 0; i < idOrdenadas.length; i++) {
+            System.out.println("Piloto: " + participantes[idOrdenadas[i]].getNombre() + "\nPuntuación: "
+                    + puntuacionPilotos[idOrdenadas[i]]);
+        }
+    }
+
+    public void imprimirResultadoEscuderia() {
+        String[] escuderias = getArrayEscuderia(participantes);
+        int[] puntuacionEscuderia = getPuntuacionEscuderia(escuderias);
+        for (int i = 0; i < escuderias.length; i++) {
+            System.out.println("Escudería: " + escuderias[i] + "\n Puntuación: " + puntuacionEscuderia[i]);
+        }
+    }
+
+    private String[] getArrayEscuderia(Piloto[] participantes) {
+        String[] escuderias = null;
+        for (Piloto iPiloto : participantes) {
+            outerLoop: if (escuderias == null) {
+                String[] buffer = new String[1];
+                buffer[0] = iPiloto.getCoche().getEscuderia();
+                escuderias = buffer;
+            } else {
+                for (String nombre : escuderias) {
+                    if (nombre.equals(iPiloto.getCoche().getEscuderia())) {
+                        break outerLoop;
+                    }
+                }
+                int length = escuderias.length + 1;
+                String[] buffer = new String[length];
+                for (int i = 0; i < escuderias.length; i++) {
+                    buffer[i] = escuderias[i];
+                }
+                buffer[length - 1] = iPiloto.getCoche().getEscuderia();
+                escuderias = buffer;
+            }
+        }
+        return escuderias;
+    }
+
+    private int[] getPuntuacionEscuderia(String[] escuderias) {
+        int[] puntuacionEscuderia = new int[escuderias.length];
+        for (int i = 0; i < participantes.length; i++) {
+            for (int j = 0; j < escuderias.length; j++) {
+                if (escuderias[j].equals(participantes[i].getCoche().getEscuderia())) {
+                    puntuacionEscuderia[j] += puntuacionPilotos[i];
+                }
+            }
+        }
+        return puntuacionEscuderia;
+    }
+
+    private int[] insertSort(int[] arrayId, int[] arrayPuntuacion) {
+        int[] sorted = arrayId;
+        for (int i = 1; i < sorted.length; i++) {
+            int key = sorted[i];
+            int j = i - 1;
+            while (j >= 0 && arrayPuntuacion[sorted[j]] < arrayPuntuacion[key]) {
+                sorted[j + 1] = sorted[j];
+                j -= 1;
+            }
+            sorted[j + 1] = key;
+        }
+        return sorted;
+    }
+
+    private void almacenarResultados(Piloto[] arrayOrdenado) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < participantes.length; j++) {
+                if (arrayOrdenado[i].getNombre().equals(participantes[j].getNombre())) {
+                    switch (j) {
+                        case 0:
+                            puntuacionPilotos[j] += 10;
+                            break;
+                        case 1:
+                            puntuacionPilotos[j] += 8;
+                            break;
+                        case 2:
+                            puntuacionPilotos[j] += 5;
+                            break;
+                    }
+                }
+            }
+        }
     }
 
     public boolean anhadirCarrera(Carrera carrera, int vueltas) {
