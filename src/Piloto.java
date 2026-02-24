3508a107fb2c5933115247cea561aa2e00e7563b
Juan Santos Cotelo <juansantoscotelo@gmail.com>
2026-02-24 13:43:40 +0100

    Hice el método para la velocidad media y cambie el metodo de getHabilidad para que mediante texto nos devuelva un numero

--- src/Piloto.java
+++ src/Piloto.java
@@ -1,10 +1,18 @@
+import java.util.Random;
+
 public class Piloto {
     private String nombre;
     private int edad;
     private Coche coche;
-    private float habilidad;
+    private String habilidad;
 
     public double velocidadMedia() {
+        Random rand = new Random();
+        double numAleatorio = rand.nextDouble(0.6,0.9);
+        //Pongo siempre menor que 1 porque en una carrera la velocidad media
+        //nunca es igual a la velocidad maxima
+        double media = coche.getVelocidadMax() * getHabilidad() * numAleatorio;
+
         return 0;
     }
 
@@ -27,7 +35,20 @@ public class Piloto {
         return coche;
     }
 
-    public float getHabilidad() {
-        return habilidad;
+    public double getHabilidad() {
+        Random rand = new Random();
+        if (habilidad.equals("Excelente")) {
+            double habAleatoria = rand.nextDouble(0.9,1);
+            return habAleatoria;
+        }
+        if (habilidad.equals("Bueno")) {
+            double habAleatoria = rand.nextDouble(0.8,0.9);
+            return habAleatoria;
+        }
+        if (habilidad.equals("Mediocre")) {
+            double habAleatoria = rand.nextDouble(0.7,0.8);
+            return habAleatoria;
+        }
+        return 0;
     }
 }
