7de648143d2345575d17f5c17944c21dcbecb01e
NoamNeo <gabriel.loira.lorenzo@gmail.com>
2026-02-26 16:48:07 +0100

    Creamos constructor y eliminamos atributos no usados junto sus get y set

--- src/Competicion.java
+++ src/Competicion.java
@@ -4,10 +4,7 @@ public class Competicion {
     private String nombre;
     private Piloto[] participantes;
     private int[] puntuacionPilotos;
-    private int numPilotos; // Contador para saber cuántos pilotos hay realmente
-
     private Carrera[] carreras;
-    private int numCarreras; // Contador para saber cuántas carreras hay
 
     public boolean arrancarCampeonato() {
         if (!haCampeonato) {
@@ -180,19 +177,12 @@ public class Competicion {
         return false;
     }
 
-    public int getNumPilotos() {
-        return numPilotos;
-    }
-
-    public void setNumPilotos(int numPilotos) {
-        this.numPilotos = numPilotos;
-    }
-
     public String getNombre() {
         return nombre;
     }
 
-    public void setNombre(String nombre) {
+    public Competicion(String nombre) {
         this.nombre = nombre;
     }
+
 }
